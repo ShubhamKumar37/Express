1be26800aa8ad521bfa30c0120cca31a982f51fc
ShubhamKumar37 <sk9818281820@gmail.com>
2024-04-16 17:51:05 +0530

    new blog app (40%) done (office)

--- blog-app-babbar/controllers/commentController.js
+++ blog-app-babbar/controllers/commentController.js
@@ -14,9 +14,9 @@ exports.createComment = async (req, res) =>
 
         // This is will take the post id from the request body and use that id to find the respective post and then update its
         // comment array by adding the current comment id to the array.
-        // $push is an operator like append function (Maybe for understanding only)
-        // {new:true} will help to return the final updated document to us
-        // exec to execute the query
+        // $push is an operator like append function (Maybe for understanding only).
+        // {new:true} will help to return the final updated document to us.
+        // exec to execute the query.
         const updatedPost = await Post.findByIdAndUpdate(post, {$push : {comment: savedComment._id}}, {new: true}).populate().exec();
 
         res.status(200).json({
