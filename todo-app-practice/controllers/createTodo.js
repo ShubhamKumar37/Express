8ec1ee6215bedbc4a2d7005fc7acaaf3576f37c3
ShubhamKumar37 <sk9818281820@gmail.com>
2024-04-18 13:40:27 +0530

    only added some comments to explain vaibhav

--- todo-app-practice/controllers/createTodo.js
+++ todo-app-practice/controllers/createTodo.js
@@ -1,11 +1,12 @@
 const Schema = require("../models/todoSchema");
-
 exports.createTodo = async (req, res) =>
 {
     try
     {
         const {title, description} = req.body;
         const response = await Schema.create({title, description});
+        // const newresponse = new Schema({title, description});
+        // const updatedresponse = newresponse.save();
         
         res.status(200).json(
             {
